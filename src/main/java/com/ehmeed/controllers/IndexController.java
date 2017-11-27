3b32b3bc333b635ff96107a4a0a5489adefeda05
Ehmeed <milanhruban@seznam.cz>
2017-11-27 01:26:33 +0100

    Adding find methods to repositories

--- src/main/java/com/ehmeed/controllers/IndexController.java
+++ src/main/java/com/ehmeed/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.ehmeed.controllers;
 
+import com.ehmeed.domain.Category;
+import com.ehmeed.domain.UnitOfMeasure;
+import com.ehmeed.repositories.CategoryRepository;
+import com.ehmeed.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM ID is : " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
