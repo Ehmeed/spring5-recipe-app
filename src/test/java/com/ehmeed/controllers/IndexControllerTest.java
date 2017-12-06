dae6d2a7a23e861b14c19de724b7a001b6e90d18
Ehmeed <milanhruban@seznam.cz>
2017-12-06 14:05:41 +0100

    Adding IndexController unit test

---
+++ src/test/java/com/ehmeed/controllers/IndexControllerTest.java
@@ -0,0 +1,41 @@
+package com.ehmeed.controllers;
+
+
+import com.ehmeed.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+        String viewName = indexController.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+
+}
