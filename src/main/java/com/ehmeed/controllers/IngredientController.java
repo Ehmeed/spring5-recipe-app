818aa9c0efdf925b966a9d9ed4ddfc233cc4c363
Ehmeed <milanhruban@seznam.cz>
2017-12-19 00:25:24 +0100

    Adding IngredientsController implementation

--- src/main/java/com/ehmeed/controllers/IngredientController.java
+++ src/main/java/com/ehmeed/controllers/IngredientController.java
@@ -3,6 +3,10 @@ package com.ehmeed.controllers;
 import com.ehmeed.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
@@ -12,5 +16,13 @@ public class IngredientController {
 
     public IngredientController(RecipeService recipeService){
         this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{id}/ingredients")
+    public String listIngredients(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/ingredients/list";
     }
 }
